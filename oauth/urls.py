4b2491bf445e9c0c50d23a6902913f4a7f019213
lcy <lichunyang@stu.xidian.edu.cn>
2016-10-18 19:42:44 +0800

    change url

--- oauth/urls.py
+++ oauth/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from . import views 
 
 urlpatterns = [
-    url(r'^github/', views.oauth_github,name='oauth_github'),
-    url(r'^tencent/', views.oauth_tencent,name='oauth_tencent'),
-    url(r'^tencent/get/', views.oauth_tencent_get,name='oauth_tencent_get'),
+    url(r'github/', views.oauth_github,name='oauth_github'),
+    url(r'tencent/', views.oauth_tencent,name='oauth_tencent'),
+    url(r'tencent/get/', views.oauth_tencent_get,name='oauth_tencent_get'),
 ]
